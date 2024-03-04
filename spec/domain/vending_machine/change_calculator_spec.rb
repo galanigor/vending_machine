2faedfb7d3b836a6cf0c1cf91ad928fd0b4f6bb8
Ihor Halan <galanigor94@gmail.com>
2024-03-04 17:04:48 +0200

    More change calculator specs

--- spec/domain/vending_machine/change_calculator_spec.rb
+++ spec/domain/vending_machine/change_calculator_spec.rb
@@ -27,4 +27,33 @@ RSpec.describe VendingMachine::ChangeCalculator do
       expect(subject).to eq([])
     end
   end
+
+  context "with not enough coins for change" do
+    let(:stored_coins) do
+      VendingMachine::AVAILABLE_COINS.zip(Array.new(VendingMachine::AVAILABLE_COINS.count) { 0 }).to_h
+    end
+
+    it "raises the NotEnoughCoinsForChange error" do
+      expect {
+        subject
+      }.to raise_error(VendingMachine::ChangeCalculator::NotEnoughCoinsForChange)
+    end
+  end
+
+  context "with some stored coins missing" do
+    before do
+      stored_coins[5] = 0
+    end
+
+    it "returns the change in expected format" do
+      expected_change = [
+        { value: 3, count: 5 },
+        { value: 1, count: 1 },
+        { value: 0.5, count: 1 },
+        { value: 0.25, count: 1 }
+      ]
+  
+      expect(subject).to match_array(expected_change)
+    end
+  end
 end
